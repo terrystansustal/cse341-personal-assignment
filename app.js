82648c4125f39494698b8b5edaddd0ab28341a51
terrystansustal <terrysustal@gmail.com>
2023-10-12 19:54:20 -0700

    Installed modules for project and app,js

---
+++ app.js
@@ -0,0 +1,26 @@
+// app.js will be the main server for this program
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connection');
+
+const port = process.env.port || 8080;
+const app = express();
+
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err)
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
+});
+
