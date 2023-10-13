e45cb724384d16c3f4d3449d20aa2ad10bbb7e36
terrystansustal <terrysustal@gmail.com>
2023-10-12 20:28:34 -0700

    app.js

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // app.js will be the main server for this program
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connection');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connection");
 
 const port = process.env.port || 8080;
 const app = express();
 
 app
-    .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
-    })
-    .use('/', require('./routes'));
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err)
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on ${port}`);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
-
