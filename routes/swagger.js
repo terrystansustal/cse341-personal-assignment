68be3977243b82275cdc0691ef8ec32ff7f28ada
terrystansustal <terrysustal@gmail.com>
2023-10-13 19:28:09 -0700

    Fixed bug

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-const router = require('.');
+const router = require('../swagger.json');
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
