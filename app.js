37d5d5f72cf0ea310563faa89a749cdbd710606d
Terrystan Sustal <terrysustal@gmail.com>
2023-10-30 16:19:05 -0700

    Fixed bug

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app
   })
 
   .get('/protected', isLoggedIn, (req, res) => {
-    res.send(`Welcome ${req.user.displayName} <a href="/students">Students</a> <a href="./routes/index.js">API DOCS</a>`);
+    res.send(`Welcome ${req.user.displayName} <a href="/students">Students</a> <a href="https://cse341-personal-assignment-uohb.onrender.com/api-docs/">API DOCS</a>`);
   })
 
   .use((req, res, next) => {
