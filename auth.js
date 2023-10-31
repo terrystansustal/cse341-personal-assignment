9e9e65ab0ace8f6b9ac2b362856a6c72de71ce4e
Terrystan Sustal <terrysustal@gmail.com>
2023-10-30 17:04:34 -0700

    Fixed a bug in auth.js

--- auth.js
+++ auth.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
+const GOOGLE_CLIENT_ID = '701305680395-r5r5pg7mupi6cjgtkroov86k7j97nrn7.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
 passport.use(new GoogleStrategy({
-    clientID:     process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "https://cse341-personal-assignment-uohb.onrender.com/google/callback",
     passReqToCallback: true
   },
