4e4026fe54c498e0a752d27a6f5d655382b621d8
Terrystan Sustal <terrysustal@gmail.com>
2023-11-02 22:06:09 -0700

    Added validation.js back

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,31 +1,31 @@
-// const validator = require('../helpers/validate');
+const validator = require('../helpers/validate');
 
-// const saveContact = (req, res, next) => {
-//     const validationRule = {
-//       firstName: "required|string",
-//       lastName: "required|string",
-//       birthDate: "required|string",
-//       homeAddress: "required|string",
-//       contactInformation: "required|string",
-//       email: "required|string",
-//       studentID: "required|string",
-//       enrollmentStatus: "required|string",
-//       academicProgram: "required|string"
-//     };
-//     validator(req.body, validationRule, {}, (err, status) => {
-//         console.log(req.body);
-//         if (!status) {
-//             res.status(412).send({
-//                 success: false,
-//                 message: 'Validation failed',
-//                 data: err
-//             });
-//         } else {
-//             next();
-//         }
-//     });
-// };
+const saveContact = (req, res, next) => {
+    const validationRule = {
+      firstName: "required|string",
+      lastName: "required|string",
+      birthDate: "required|string",
+      homeAddress: "required|string",
+      contactInformation: "required|string",
+      email: "required|string",
+      studentID: "required|string",
+      enrollmentStatus: "required|string",
+      academicProgram: "required|string"
+    };
+    validator(req.body, validationRule, {}, (err, status) => {
+        console.log(req.body);
+        if (!status) {
+            res.status(412).send({
+                success: false,
+                message: 'Validation failed',
+                data: err
+            });
+        } else {
+            next();
+        }
+    });
+};
 
-// module.exports = {
-//     saveContact
-// };
+module.exports = {
+    saveContact
+};
